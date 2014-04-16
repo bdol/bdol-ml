49f318ffeebd3ba8b037248dc49e748186ae5264
Brian Dolhansky <bdol@hoothoot.(none)>
2014-04-16 13:34:21 -0700

    Checking in a buggy version of the MLP code for backup purposes

--- neural_networks/mlp_mnist_full/mlp_mnist_full.py
+++ neural_networks/mlp_mnist_full/mlp_mnist_full.py
@@ -1,8 +1,9 @@
+import ConfigParser
 import ast
+import bdolpyutils as bdp
 import numpy as np
 import sys
-import bdolpyutils as bdp
-import ConfigParser
+import time
 
 def rectified_linear(X):
   leqZeroIdx = X<=0
@@ -10,13 +11,40 @@ def rectified_linear(X):
   Xprime[leqZeroIdx] = 0
   return Xprime
 
-# I think this is wrong?
 def d_rectified_linear(X):
-  leqZeroIdx = X<=0
-  return leqZeroIdx*1
+  gZeroIdx = X>0
+  return gZeroIdx*1
+
+def softmax(X):
+  # TODO: there's gotta be a better way to do this without all the reshapes...
+  # Use the log-sum-exp trick for numerical stability
+  m = np.amax(X, axis=1)
+  m = np.reshape(m, (len(m), -1))
+  y_exp = np.exp(X-m)
+
+  s = np.sum(y_exp, axis=1)
+  s = np.reshape(s, (len(s), -1))
+  try:
+    r = y_exp/s
+  except FloatingPointError:
+    print y_exp
+    sys.exit(1)
+  return r
+
+def d_softmax(X):
+  # TODO: I don't think this is needed at the output layer, you should go
+  # through the math
+  return X
 
 def sigmoid(X):
-  return 1/(1+np.exp(-X))
+  r = 0
+  try:
+    r = 1/(1+np.exp(-X))
+  except FloatingPointError:
+    print X
+    sys.exit(1)
+
+  return r
 
 def d_sigmoid(X):
   return sigmoid(X)*(1-sigmoid(X))
@@ -32,15 +60,17 @@ class Layer:
     self.d_activation = d_activation
     self.z = np.zeros((size[0], size[1]))
     self.d = np.zeros((size[0], size[1]))
+    self.a = 0
 
   def compute_activation(self, X, doDropout=False, dropoutProb=0.5):
     X_d = X
+    # I think you should drop out columns here?
     if doDropout:
-      X_d = X_d*np.random.binomial(1, dropoutProb, X_d.shape)
+      X_d = X_d*np.random.binomial(1, (1-dropoutProb), X_d.shape)
 
-    s = X_d.dot(self.W)
-    self.z = self.activation(s)
-    self.d = self.d_activation(self.z)
+    self.a = X_d.dot(self.W)
+    self.z = self.activation(self.a)
+    #self.d = self.d_activation(s)
 
     return self.z
 
@@ -54,9 +84,11 @@ class MLP:
     # Activations map - we need this to map from the strings in the *.ini file
     # to actual function names
     activationsMap = {'sigmoid': sigmoid,
-                      'rectified_linear': rectified_linear}
+                      'rectified_linear': rectified_linear,
+                      'softmax': softmax}
     d_activationsMap = {'sigmoid': d_sigmoid,
-                        'rectified_linear': d_rectified_linear}
+                        'rectified_linear': d_rectified_linear,
+                        'softmax': d_softmax}
 
     # Initialize each layer with the given parameters
     self.layers = []
@@ -99,18 +131,31 @@ class MLP:
 
     # Backpropagate for each training example separately
     for j in range(0, Y.shape[1]):
-      deltas = []
-      for i in range(len(self.layers)-1, -1, -1):
-        # Construct the matrix of derivatives D
-        D = np.diag(self.layers[i].d[j, :])
-        # We are at the output layer
-        if i==len(self.layers)-1:
-          deltas.append( D.dot(e[j, :].T) )
-          deltas[0] = np.reshape(deltas[0], (len(deltas[0]), -1))
-        # We are not at the output layer, so delta is scaled by the weights
-        if i<len(self.layers)-1:
-          W = self.layers[i+1].W[0:-1, :]
-          deltas.insert(0, D.dot(W).dot(deltas[0]))
+      # delta_k's at the output layer
+      deltas = [np.reshape(e[j, :], (len(e[j, :]), -1))]
+      for i in range(len(self.layers)-2, -1, -1):
+        W = self.layers[i+1].W
+        n_units_j = self.layers[i].W.shape[1]
+        n_units_k = self.layers[i+1].W.shape[1]
+        deltas.insert(0, np.zeros((n_units_j, 1)))
+        for jj in range(0, n_units_j):
+          s = 0
+          a_j = self.layers[i].a[j, jj]
+          d = self.layers[i].d_activation(a_j)
+          for kk in range(0, n_units_k):
+            s += W[jj, kk]*deltas[1][kk]
+          deltas[0][jj] = d*s
+
+        ## Construct the matrix of derivatives D
+        #D = np.diag(self.layers[i].d[j, :])
+        ## We are at the output layer
+        #if i==len(self.layers)-1:
+          #deltas.append( e[j, :].T )
+          #deltas[0] = np.reshape(deltas[0], (len(deltas[0]), -1))
+        ## We are not at the output layer, so delta is scaled by the weights
+        #if i<len(self.layers)-1:
+          #W = self.layers[i+1].W[0:-1, :]
+          #deltas.insert(0, D.dot(W).dot(deltas[0]))
 
       # Update the gradients
       for i in range(0, len(self.layers)):
@@ -127,7 +172,8 @@ class MLP:
 
     # Update the weights
     for i in range(0, len(self.layers)):
-      self.currentGrad[i] = momentum*self.previousGrad[i]-(1-momentum)*eta*W_grad[i]
+      self.currentGrad[i] = -eta*W_grad[i]
+      #self.currentGrad[i] = momentum*self.previousGrad[i]-(1-momentum)*eta*W_grad[i]
       self.layers[i].W += self.currentGrad[i]
       self.previousGrad[i] = self.currentGrad[i]
 
@@ -159,13 +205,9 @@ def RMSE(Y, Yhat):
   return np.sqrt(1/float(Y.shape[0])*rmse)[0]
 
 def numErrs(Y, Yhat):
-  errs = 0
-  for i in range(0, Y.shape[0]):
-    y_i = np.where(Y[i, :])[0]
-    yhat = np.argmax(Yhat[i, :])
-    if y_i != yhat:
-      errs += 1
-  return errs
+  Y_idx = np.argmax(Y, axis=1)
+  Yhat_idx = np.argmax(Yhat, axis=1)
+  return np.sum(Y_idx != Yhat_idx)
 
 if __name__ == "__main__":
   # Load the parameters for this network from the initialization file
@@ -192,6 +234,19 @@ if __name__ == "__main__":
   minibatchSize = ast.literal_eval(cfg.get('experiment', 'minibatchSize'))
   learningRate = ast.literal_eval(cfg.get('experiment', 'learningRate'))
   rateDecay = ast.literal_eval(cfg.get('experiment', 'rateDecay'))
+  np.random.seed(1234)
+  np.seterr(all='raise')
+  np.seterr(under='ignore')
+
+  # Set up the program options
+  debugMode = ast.literal_eval(cfg.get('program', 'debugMode'))
+  logToFile = ast.literal_eval(cfg.get('program', 'logToFile'))
+  logFileBaseName = cfg.get('program', 'logFileBaseName')
+  if logToFile:
+    dateStr = time.strftime('_%Y-%m-%d_%H-%M')
+    logFile = logFileBaseName+dateStr+".txt"
+    f = open(logFile, "w")
+    f.write('RMSE Train,Num. Errors Train,RMSE Valid.,Num. Errors Valid,RMSE Test,Num. Errors Test,learningRate,momentum,elapsedTime\n')
 
   # Load the corresponding data
   X_tr, Y_tr, X_v, Y_v, X_te, Y_te = bdp.loadMNIST(mnistPath, digits=digits,
@@ -200,6 +255,8 @@ if __name__ == "__main__":
   print "Training for "+str(numEpochs)+" epochs:"
   p = momentumInitial
   for t in range(0, numEpochs):
+    startTime = time.time()
+
     if t< momentumT:
       p = 1/momentumT*momentumInitial + (1-1/momentumT)*momentumFinal
     else:
@@ -212,6 +269,8 @@ if __name__ == "__main__":
       if i%100==0:
         bdp.progBar(i, X_tr.shape[0])
     bdp.progBar(X_tr.shape[0], X_tr.shape[0])
+    elapsedTime = (time.time()-startTime)
+    print " Epoch {0}, learning rate: {1:.4f}, elapsed time: {2:.2f}s".format(t, learningRate, elapsedTime)
 
     learningRate = learningRate*rateDecay
 
@@ -225,12 +284,23 @@ if __name__ == "__main__":
     YhatTest = mlp.test(X_te)
     rmseErrorTest = RMSE(Y_te, YhatTest)
     numErrsTest = numErrs(Y_te, YhatTest)
-    print
-    print "Train RMSE: {0}\tTrain errors: {1}".format(rmseErrorTrain,
+
+    errsStr = "Train RMSE: {0}\tTrain errors: {1}\n".format(rmseErrorTrain,
         numErrsTrain)
-    print "Valid. RMSE: {0}\tValid. errors: {1}".format(rmseErrorValid,
+    errsStr += "Valid. RMSE: {0}\tValid. errors: {1}\n".format(rmseErrorValid,
         numErrsValid)
-    print "Test RMSE: {0}\tTest errors: {1}".format(rmseErrorTest,
+    errsStr += "Test RMSE: {0}\tTest errors: {1}\n".format(rmseErrorTest,
         numErrsTest)
-    print
+    print errsStr
+
+    logStr = "{0},{1},{2},{3},{4},{5},{6},{7},{8:.2f}\n".format(
+                rmseErrorTrain, numErrsTrain,
+                rmseErrorValid, numErrsValid,
+                rmseErrorTest, numErrsTest,
+                learningRate, p,
+                elapsedTime)
+    if logToFile:
+      f.write(logStr)
     
+  if logToFile:
+    f.close()
