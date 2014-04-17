Fixed a gradient bug, but the SGD still isn't working properly

neural_networks/mlp_mnist_full/mlp_mnist_full.py
@@ -16,7 +16,6 @@ def d_rectified_linear(X):
   return gZeroIdx*1
 
 def softmax(X):
-  # TODO: there's gotta be a better way to do this without all the reshapes...
   # Use the log-sum-exp trick for numerical stability
   m = np.amax(X, axis=1)
   m = np.reshape(m, (len(m), -1))
@@ -24,16 +23,16 @@ def softmax(X):
 
   s = np.sum(y_exp, axis=1)
   s = np.reshape(s, (len(s), -1))
-  try:
-    r = y_exp/s
-  except FloatingPointError:
-    print y_exp
-    sys.exit(1)
-  return r
+  #try:
+    #r = y_exp/s
+  #except FloatingPointError:
+    #print y_exp
+    #print "softmax err"
+    #sys.exit(1)
+  return y_exp/s
 
 def d_softmax(X):
-  # TODO: I don't think this is needed at the output layer, you should go
-  # through the math
+  #TODO: this is wrong
   return X
 
 def sigmoid(X):
@@ -41,7 +40,9 @@ def sigmoid(X):
   try:
     r = 1/(1+np.exp(-X))
   except FloatingPointError:
+    print "xerr"
     print X
+    print "Sigmoid error"
     sys.exit(1)
 
   return r
@@ -70,7 +71,7 @@ class Layer:
 
     self.a = X_d.dot(self.W)
     self.z = self.activation(self.a)
-    #self.d = self.d_activation(s)
+    self.d = self.d_activation(self.a)
 
     return self.z
 
@@ -98,6 +99,8 @@ class MLP:
       size = [layerSizes[i]+1, layerSizes[i+1]]
       activation = activationsMap[activations[i]]
       d_activation = d_activationsMap[activations[i]]
+      print "Layer {0}, activation {1}, d_activation {2}, size {3}".format(i,
+          activation, d_activation, size)
 
       l = Layer(size, activation, d_activation)
       self.layers.append(l)
@@ -105,78 +108,86 @@ class MLP:
       self.previousGrad.append(np.zeros(size))
 
   def forward_propagate(self, X):
-    currentInput = np.atleast_2d(X)
-    # First do forward propagation with the input data
+    x_l = np.atleast_2d(X)
     for i in range(0, len(self.layers)):
-      # Append a 1 to the current input for the bias term
-      currentInput = np.append(currentInput, np.ones((currentInput.shape[0], 1)), 1)
-      # Now actually compute the activation
-      # Input layer
-      if i==0:
-        currentInput = self.layers[i].compute_activation(currentInput,
-            self.doDropout, self.dropoutInputProb)
-      else:
-        currentInput = self.layers[i].compute_activation(currentInput,
-          self.doDropout, self.dropoutProb)
-    return currentInput
+      x_l = np.append(x_l, np.ones((x_l.shape[0], 1)), 1)
+      x_l = self.layers[i].compute_activation(x_l)
 
-  def backpropagate(self, output, X, Y, eta, momentum):
-    # TODO: make the error function an input parameter. It's cross entropy in
-    # the Hinton paper
-    e = output-Y
+    return x_l
+
+  def xent_cost(self, X, Y, Yhat):
+    E = 0
+    for i in range(0, Y.shape[0]):
+      y = np.argmax(Y[i, :])
+      E -= np.log(Yhat[i, y])
+    return E
+
+  def check_gradient(self, X, Y, eta, momentum):
+    eps = 1E-4
+    output = self.forward_propagate(X)
+    W_grad = self.calculate_gradient(output, X, Y, eta, momentum)
+    zzz = self.layers[0].z[0, 0]
+    
+    W_initial = []
+    for i in range(0, len(self.layers)):
+      W_initial.append(np.copy(self.layers[i].W))
+
+    for i in range(0, len(self.layers)):
+      W = self.layers[i].W
+      for j in range(0, W.shape[0]):
+        for k in range(0, W.shape[1]):
+          self.layers[i].W[j,k] += eps
+          out_p = self.forward_propagate(X)
+          E_p = self.xent_cost(X, Y, out_p)
+          self.layers[i].W[j,k] = W_initial[i][j,k]
+          self.layers[i].W[j,k] -= eps 
+          out_m = self.forward_propagate(X)
+          E_m = self.xent_cost(X, Y, out_m)
+          self.layers[i].W[j,k] = W_initial[i][j,k]
+
+          g_approx = (E_p-E_m)/(2*eps)
+          if abs(g_approx-W_grad[i][j,k])>1E-4:
+            print "Gradient checking failed for ",i,j,k,abs(g_approx-W_grad[i][j,k])
+
+  def calculate_gradient(self, output, X, Y, eta, momentum):
     # First set up the gradients
     W_grad = []
+    deltas = []
     for i in range(0, len(self.layers)):
       W_grad.append( np.zeros(self.layers[i].W.shape) )
+      deltas.append( np.zeros((self.layers[i].W.shape[1], 1)))
+
+    e = output-Y
 
     # Backpropagate for each training example separately
-    for j in range(0, Y.shape[1]):
-      # delta_k's at the output layer
-      deltas = [np.reshape(e[j, :], (len(e[j, :]), -1))]
+    for j in range(0, Y.shape[0]):
+      # Delta at the output layer
+      deltas[-1] = e[j, :].T
+      # Backpropagation to determine the deltas at each layer
       for i in range(len(self.layers)-2, -1, -1):
-        W = self.layers[i+1].W
-        n_units_j = self.layers[i].W.shape[1]
-        n_units_k = self.layers[i+1].W.shape[1]
-        deltas.insert(0, np.zeros((n_units_j, 1)))
-        for jj in range(0, n_units_j):
-          s = 0
-          a_j = self.layers[i].a[j, jj]
-          d = self.layers[i].d_activation(a_j)
-          for kk in range(0, n_units_k):
-            s += W[jj, kk]*deltas[1][kk]
-          deltas[0][jj] = d*s
+        D = np.diag(self.layers[i].d[j, :])
+        W = self.layers[i+1].W[0:-1, :]
+        deltas[i] = D.dot(W.dot(deltas[i+1]))
 
-        ## Construct the matrix of derivatives D
-        #D = np.diag(self.layers[i].d[j, :])
-        ## We are at the output layer
-        #if i==len(self.layers)-1:
-          #deltas.append( e[j, :].T )
-          #deltas[0] = np.reshape(deltas[0], (len(deltas[0]), -1))
-        ## We are not at the output layer, so delta is scaled by the weights
-        #if i<len(self.layers)-1:
-          #W = self.layers[i+1].W[0:-1, :]
-          #deltas.insert(0, D.dot(W).dot(deltas[0]))
-
-      # Update the gradients
+      # Add the gradient for this example
       for i in range(0, len(self.layers)):
-        # We are at the input layer
         z_i = 0
+        # Gradient at the input layer
         if i==0:
           z_i = X[j, :]
         else:
           z_i = self.layers[i-1].z[j, :]
-
         z_i = np.append(z_i, [1])
-        z_i = np.reshape(z_i, (-1, len(z_i)))
-        W_grad[i] += np.outer(deltas[i], z_i).T
+        W_grad[i] += np.outer(z_i, deltas[i])
 
-    # Update the weights
+    return W_grad
+
+  def backpropagate(self, output, X, Y, eta, momentum):
+    W_grad = self.calculate_gradient(output, X, Y, eta, momentum)
+
+    # Take a step in the direction of the gradient
     for i in range(0, len(self.layers)):
-      self.currentGrad[i] = -eta*W_grad[i]
-      #self.currentGrad[i] = momentum*self.previousGrad[i]-(1-momentum)*eta*W_grad[i]
-      self.layers[i].W += self.currentGrad[i]
-      self.previousGrad[i] = self.currentGrad[i]
-
+      self.layers[i].W -= eta*W_grad[i]
       # Constrain the weights going to the hidden units if necessary
       if i<len(self.layers)-1:
         wLens = np.linalg.norm(self.layers[i].W, axis=0)**2
@@ -234,6 +245,7 @@ if __name__ == "__main__":
   minibatchSize = ast.literal_eval(cfg.get('experiment', 'minibatchSize'))
   learningRate = ast.literal_eval(cfg.get('experiment', 'learningRate'))
   rateDecay = ast.literal_eval(cfg.get('experiment', 'rateDecay'))
+  checkGradient = ast.literal_eval(cfg.get('experiment', 'rateDecay'))
   np.random.seed(1234)
   np.seterr(all='raise')
   np.seterr(under='ignore')
@@ -251,6 +263,11 @@ if __name__ == "__main__":
   # Load the corresponding data
   X_tr, Y_tr, X_v, Y_v, X_te, Y_te = bdp.loadMNIST(mnistPath, digits=digits,
       asBitVector=True)
+
+  if checkGradient:
+    print "Checking gradient..."
+    mlp.check_gradient(X_tr[0:10, :], Y_tr[0:10, :], learningRate, 0)
+    print "Gradient is correct!"
 
   print "Training for "+str(numEpochs)+" epochs:"
   p = momentumInitial
@@ -284,6 +301,9 @@ if __name__ == "__main__":
     YhatTest = mlp.test(X_te)
     rmseErrorTest = RMSE(Y_te, YhatTest)
     numErrsTest = numErrs(Y_te, YhatTest)
+
+    np.savetxt('mnistyhat.txt', YhatTrain)
+    np.savetxt('mnisty.txt', Y_tr)
 
     errsStr = "Train RMSE: {0}\tTrain errors: {1}\n".format(rmseErrorTrain,
         numErrsTrain)