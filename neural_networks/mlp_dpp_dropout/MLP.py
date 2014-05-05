3e266738005ea2895c94861c9af47e915fdc4403
Brian Dolhansky <bdolmail@gmail.com>
2014-05-05 14:10:51 -0700

    incremental update to allow continuation of experiments (not done yet)

--- neural_networks/mlp_dpp_dropout/MLP.py
+++ neural_networks/mlp_dpp_dropout/MLP.py
@@ -1,5 +1,29 @@
-import numpy as np
+import ConfigParser
+import ast
+import cPickle as pickle
 import dpp
+import numpy as np
+import shutil
+import sys
+import time
+import uuid
+
+def save_state(model, params, modelFilename, paramsFilename):
+  pickle.dump(model, open(modelFilename, "wb"))
+  pickle.dump(model, open(paramsFilename, "wb"))
+
+  return 0
+
+def load_state(modelFilename, paramsFilename):
+  model = pickle.load(open(modelFilename, "rb"))
+  params = pickle.load(open(experimentFilename, "rb"))
+  return (model, params)
+
+def get_help():
+  '''
+  Returns a string for help about this class.
+  '''
+  pass
 
 def rectified_linear(X):
   return np.maximum(X, 0.0)
@@ -29,6 +53,174 @@ def d_sigmoid(X):
 def linear(X):
   return X
 
+class Parameters:
+  '''
+  This class contains the current experiment parameters, including:
+    Model parameters:
+      layerSizes: A list of layer sizes
+      activations: The activation type for each layer
+      dropoutType
+      dropoutProb
+      dropoutInputProb
+      wLenLimit: The hard limit to a weight vector's L2 norm
+
+    Experiment parameters:
+      checkGradient: True/False, whether or not to check gradient
+      checkGradientEpochs: List of epochs to check the gradient at
+      continueFromSaved: Whether or not to continue from a saved state
+      currentEpoch
+      currentLearningRate
+      digits
+      initialLearningRate
+      minibatchSize
+      momentumCurrent
+      momentumFinal
+      momentumInitial
+      momentumT: Specifies the epoch number when momentum=momentumFinal
+      name: A descriptive name for the experiment
+      rateDecay
+      totalEpochs
+
+    Program parameters:
+      logToFile: True/False, whether to log experiment results to a file
+      logFile: The name of the log file (if applicable)
+      saveState: True/False, whether or not to save the state at a given time
+      saveStateInterval: Save the model every x epochs
+      saveStateUnique: Save a unique version of the model at each point
+      saveStateBaseName: The base of the saved state filename 
+
+  To initialize an experiment, pass it a params.ini file specifying the value
+  for each option. An example params.ini file is given in this directory.
+  '''
+  def __init__(self, paramsFilename=None):
+    # Model parameters
+    self.layerSizes = None
+    self.activations = None
+    self.dropoutType = None
+    self.dropoutProb = None
+    self.dropoutInputProb = None
+    self.wLenLimit = None
+
+    # Experiment parameters
+    self.checkGradient = None
+    self.checkGradientEpochs = None
+    self.currentEpoch = None
+    self.currentLearningRate = None
+    self.datasetPath = None
+    self.initialLearningRate = None
+    self.minibatchSize = None
+    self.momentumCurrent = None
+    self.momentumFinal = None
+    self.momentumInitial = None
+    self.momentumT = None
+    self.name = None
+    self.rateDecay = None
+    self.totalEpochs = None
+
+    # Program parameters
+    self.continueFromSaved = None
+    self.logToFile = None
+    self.logFile = None
+    self.saveState = None
+    self.saveStateInterval = None
+    self.saveStateUnique = None
+    self.saveStateBaseName = None
+
+    if paramsFilename:
+      self.parseParamsFile(paramsFilename)
+
+  def parseParamsFile(self, paramsFilename):
+    '''
+    Initializes all members based on the values in the given file.
+    '''
+    cfg = ConfigParser.ConfigParser()
+    cfg.read(paramsFilename)
+
+    # Model parameters
+    self.layerSizes = list(ast.literal_eval(cfg.get('model', 'layerSizes')))
+    self.activations = cfg.get('model', 'activations').split(',')
+    self.dropoutType = cfg.get('model', 'dropoutType')
+    if self.dropoutType == 'nodropout':
+      self.doDropout = False
+    else:
+      self.doDropout = True
+    self.dropoutProb = ast.literal_eval(cfg.get('model', 'dropoutProb'))
+    self.dropoutInputProb = ast.literal_eval(cfg.get('model', 'dropoutInputProb'))
+    self.wLenLimit = ast.literal_eval(cfg.get('model', 'wLenLimit'))
+
+    # Experiment parameters
+    self.checkGradient = ast.literal_eval(cfg.get('experiment', 'checkGradient'))
+    self.checkGradientEpochs = list(ast.literal_eval(cfg.get('experiment',
+      'checkGradientEpochs')))
+    self.digits = list(ast.literal_eval(cfg.get('experiment', 'digits')))
+    self.learningRate = ast.literal_eval(cfg.get('experiment', 'learningRate'))
+    self.currentLearningRate = self.learningRate
+    self.minibatchSize = ast.literal_eval(cfg.get('experiment', 'minibatchSize'))
+    self.datasetPath = cfg.get('experiment', 'datasetPath')
+
+    self.momentumFinal = ast.literal_eval(cfg.get('experiment', 'momentumFinal'))
+    self.momentumInitial = ast.literal_eval(cfg.get('experiment', 'momentumInitial'))
+    self.momentumCurrent = self.momentumInitial
+    self.momentumT = ast.literal_eval(cfg.get('experiment', 'momentumT'))
+
+    self.name = cfg.get('experiment', 'name')
+    self.rateDecay = ast.literal_eval(cfg.get('experiment', 'rateDecay'))
+    self.currentEpoch = 0
+    self.totalEpochs = ast.literal_eval(cfg.get('experiment', 'totalEpochs'))
+
+    # Program parameters
+    self.logToFile = ast.literal_eval(cfg.get('program', 'logToFile'))
+    self.logFileBaseName = cfg.get('program', 'logFileBaseName')
+    if self.logToFile:
+      dateStr = time.strftime('%Y-%m-%d_%H-%M')
+      # Add a UUID so we can track this experiment
+      uuidStr = str(uuid.uuid1())
+      self.logFile = self.logFileBaseName+self.name+"_"+dateStr+"_"+uuidStr+".txt"
+      print self.logFile
+      self.logF = open(self.logFile, "w")
+      self.logF.write('Num. Errors Train,Num. Errors Test,learningRate,momentum,elapsedTime\n')
+      # Also copy the params over for posterity
+      paramsCopyStr = self.logFileBaseName+"params_"+str(uuidStr)+".ini"
+      shutil.copyfile(paramsFilename, paramsCopyStr)
+
+  def update(self):
+    '''
+    Updates all the paramters for the next epoch.
+    '''
+    self.currentLearningRate = self.currentLearningRate*self.rateDecay
+    if self.currentEpoch < self.momentumT:
+      self.momentumCurrent = (1.0-float(t)/self.momentumT)*self.momentumInitial + (float(t)/self.momentumT)*self.momentumFinal
+    else:
+      self.momentumCurrent = self.momentumFinal
+
+  def log(self, logStr):
+    self.logF.write(logStr)
+    self.logF.flush()
+
+  def cleanup(self):
+    self.logF.close()
+
+  def __str__(self):
+    s = "{:<40}{:<10}\n".format("Name", self.name)
+    s += "{:<40}{:<10}\n".format("Layer sizes", str(self.layerSizes))
+    s += "{:<40}{:<10}\n".format("Activations", str(self.activations))
+    s += "{:<40}{:<10}\n".format("Dropout type", str(self.dropoutType))
+    s += "{:<40}{:<10}\n".format("Dropout probability", str(self.dropoutProb))
+    s += "{:<40}{:<10}\n".format("Input dropout probability", str(self.dropoutInputProb))
+    s += "{:<40}{:<10}\n".format("Check gradient?", str(self.checkGradient))
+    s += "{:<40}{:<10}\n".format("Check gradient epochs", str(self.checkGradientEpochs))
+    s += "{:<40}{:<10}\n".format("MNIST digits", str(self.digits))
+    s += "{:<40}{:<10}\n".format("Learning rate", str(self.learningRate))
+    s += "{:<40}{:<10}\n".format("Minibatch size", str(self.minibatchSize))
+    s += "{:<40}{:<10}\n".format("Dataset path", str(self.datasetPath))
+    s += "{:<40}{:<10}\n".format("Initial momentum", str(self.momentumInitial))
+    s += "{:<40}{:<10}\n".format("Final momentum", str(self.momentumFinal))
+    s += "{:<40}{:<10}\n".format("Momentum T", str(self.momentumT))
+    s += "{:<40}{:<10}\n".format("Rate decay", str(self.rateDecay))
+    s += "{:<40}{:<10}\n".format("Total epochs", str(self.totalEpochs))
+    s += "{:<40}{:<10}\n".format("Weight limit", str(self.wLenLimit))
+
+    return s
 
 class Layer:
   def __init__(self, size, activation, d_activation):
@@ -39,7 +231,7 @@ class Layer:
     self.d_activation = d_activation
     self.z = np.zeros((size[0], size[1]))
     self.d = np.zeros((size[0], size[1]))
-    self.a = 0
+    self.a = np.zeros((1, size[1]))
 
   def random_dropout(self, dropoutProb):
     if dropoutProb == 0:
@@ -98,8 +290,6 @@ class Layer:
     else:
       self.random_dropout(dropoutProb)
     
-    #print dpp.analyze_bDPP(D)
-
   def dpp_dropout_norm(self, dropoutProb):
     if dropoutProb == 0:
       return
@@ -142,6 +332,7 @@ class Layer:
     d_idx[J.astype(int)] = 1
   
     self.prevZ[:, 0:-1] = self.prevZ[:, 0:-1]*d_idx.T
+
   def compute_activation(self, X, doDropout=False, dropoutProb=0.5,
       testing=False, dropoutSeed=None):
     self.prevZ = np.copy(X)
@@ -149,7 +340,7 @@ class Layer:
     if doDropout:
       # We are not testing, so do dropout
       if not testing:
-	self.dropoutFunction(dropoutProb)
+        self.dropoutFunction(dropoutProb)
         self.a = self.prevZ.dot(self.W)
 
       # We are testing, so we don't do dropout but we do scale the weights
@@ -165,13 +356,13 @@ class Layer:
     return self.z
 
 class MLP:
-  def __init__(self, layerSizes, activations, doDropout=False,
-      dropoutType='nodropout', dropoutProb=0.5,
-      dropoutInputProb=0.2, wLenLimit=15):
-    self.doDropout = doDropout
-    self.dropoutProb = dropoutProb
-    self.dropoutInputProb = dropoutInputProb
-    self.wLenLimit = wLenLimit
+  def __init__(self, params):
+
+    self.params = params
+    self.doDropout = params.doDropout
+    self.dropoutProb = params.dropoutProb
+    self.dropoutInputProb = params.dropoutInputProb
+    self.wLenLimit = params.wLenLimit
 
     # Activations map - we need this to map from the strings in the *.ini file
     # to actual function names
@@ -184,10 +375,10 @@ class MLP:
     # Initialize each layer with the given parameters
     self.layers = []
     self.currentGrad = []
-    for i in range(0, len(layerSizes)-1):
-      size = [layerSizes[i]+1, layerSizes[i+1]]
-      activation = activationsMap[activations[i]]
-      d_activation = d_activationsMap[activations[i]]
+    for i in range(0, len(params.layerSizes)-1):
+      size = [params.layerSizes[i]+1, params.layerSizes[i+1]]
+      activation = activationsMap[params.activations[i]]
+      d_activation = d_activationsMap[params.activations[i]]
 
       l = Layer(size, activation, d_activation)
       dropoutTypeMap = {'nodropout': None,
@@ -199,7 +390,7 @@ class MLP:
                         'dpp_dropin_EY': l.dpp_dropin_EY,
                         'random': l.random_dropout}
 
-      l.dropoutFunction = dropoutTypeMap[dropoutType]
+      l.dropoutFunction = dropoutTypeMap[params.dropoutType]
       self.layers.append(l)
       self.currentGrad.append(np.zeros(size))
 
@@ -232,16 +423,13 @@ class MLP:
       
     return E
 
-  def check_gradient(self, X, Y, eta, momentum):
+  def check_gradient(self, X, Y):
     eps = 1E-4
     dropoutSeeds = [232, 69, 75, 333]
     output = self.forward_propagate(X, dropoutSeeds=dropoutSeeds)
-    W_grad = self.calculate_gradient(output, X, Y, eta, momentum)
+    W_grad = self.calculate_gradient(output, X, Y, params.currentLearningRate,
+        params.momentumCurrent)
 
-    for i in range(0, len(self.layers)):
-      np.savetxt("W_grad_"+str(i), W_grad[i])
-      np.savetxt("W_"+str(i), self.layers[i].W)
-    
     W_initial = []
     for i in range(0, len(self.layers)):
       W_initial.append(np.copy(self.layers[i].W))
@@ -272,7 +460,7 @@ class MLP:
         bdp.progBar(j, self.layers[i].W.shape[0])
       print layer_err
 
-  def calculate_gradient(self, output, X, Y, eta, momentum):
+  def calculate_gradient(self, output, X, Y):
     # First set up the gradients
     W_grad = []
     for i in range(0, len(self.layers)):
@@ -291,12 +479,14 @@ class MLP:
 
     return W_grad
 
-  def backpropagate(self, output, X, Y, eta, momentum):
-    W_grad = self.calculate_gradient(output, X, Y, eta, momentum)
+  def backpropagate(self, output, X, Y):
+    W_grad = self.calculate_gradient(output, X, Y)
 
     # Update the current gradient, and step in that direction
     for i in range(0, len(self.layers)):
-      self.currentGrad[i] = momentum*self.currentGrad[i] - (1.0-momentum)*eta*W_grad[i]
+      p = self.params.momentumCurrent
+      eta = self.params.currentLearningRate
+      self.currentGrad[i] = p*self.currentGrad[i] - (1.0-p)*eta*W_grad[i]
       self.layers[i].W += self.currentGrad[i]
       #self.previousGrad[i] = np.copy(self.currentGrad[i])
 
@@ -308,9 +498,9 @@ class MLP:
 
   # Propagate forward through the network, record the training error, train the
   # weights with backpropagation
-  def train(self, X, Y, eta, momentum):
+  def train(self, X, Y):
     output = self.forward_propagate(X, testing=False)
-    self.backpropagate(output, X, Y, eta, momentum)
+    self.backpropagate(output, X, Y)
 
   # Just pass the data forward through the network and return the predictions
   # for the given miniBatch
--- neural_networks/mlp_dpp_dropout/mlp_dpp_dropout.py
+++ neural_networks/mlp_dpp_dropout/mlp_dpp_dropout.py
@@ -1,4 +1,5 @@
 import ConfigParser
+import argparse
 import ast
 import bdolpyutils as bdp
 import MLP
@@ -21,124 +22,171 @@ def numErrs(Y, Yhat):
   return np.sum(Y_idx != Yhat_idx)
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-i", "--ini_file", type=str, help="initialize with the given .ini file")
+  parser.add_argument("-c", "--continue_experiment", action="store_true", help="continue with the given model and experiment files")
+  parser.add_argument("-m", "--model_file", type=str, help="the model pickle to be used")
+  parser.add_argument("-p", "--param_file", type=str, help="the parameter pickle to be used")
+  parser.add_argument("-e", "--total_epochs", type=int, help="the new total epoch limit to use")
 
-  # Load the parameters for this network from the initialization file
-  cfg = ConfigParser.ConfigParser()
-  cfg.read(sys.argv[1])
+  args = parser.parse_args()
+  model = None
+  params = None
 
-  layerSizes = list(ast.literal_eval(cfg.get('net', 'layerSizes')))
-  activations = cfg.get('net', 'activations').split(',')
-  dropoutType = cfg.get('net', 'dropoutType')
-  if dropoutType == 'nodropout':
-    doDropout = False
-  else:
-    doDropout = True
-  dropoutProb = ast.literal_eval(cfg.get('net', 'dropoutProb'))
-  dropoutInputProb = ast.literal_eval(cfg.get('net', 'dropoutInputProb'))
-  wLenLimit = ast.literal_eval(cfg.get('net', 'wLenLimit'))
-  momentumInitial = ast.literal_eval(cfg.get('net', 'momentumInitial'))
-  momentumFinal = ast.literal_eval(cfg.get('net', 'momentumFinal'))
-  momentumT = ast.literal_eval(cfg.get('net', 'momentumT'))
+  if args.ini_file:
+    params = MLP.Parameters(args.ini_file)
+    print "Initialized new experiment and model with parameters:"
+    print "=============================================================="
+    print params
 
-  mlp = MLP.MLP(layerSizes, 
-      activations, 
-      doDropout=doDropout, 
-      dropoutType=dropoutType, 
-      dropoutProb=dropoutProb, 
-      dropoutInputProb=dropoutInputProb,
-      wLenLimit=wLenLimit)
+    model = MLP.MLP(params)
+
+
+  elif args.continue_experiment:
+    if not args.model_file:
+      print "Error: continue experiment specified, but no model file given!"
+      sys.exit(1)
+    if not args.param_file:
+      print "Error: continue experiment specified, but no experiment file given!"
+      sys.exit(1)
+
+      (model, params) = load_state(args.model_file, args.param_file)
+      print "Loaded existing experiment and model with parameters:"
+      print "=============================================================="
+      print params
+
+    if args.total_epochs:
+      params.totalEpochs = total_epochs
+      print "Updated total number of experiment epochs to {0}.".format(total_epochs)
 
   # Additionally load the experiment parameters
-  digits = list(ast.literal_eval(cfg.get('experiment', 'digits')))
-  mnistPath = cfg.get('experiment', 'mnistPath')
-  numEpochs = ast.literal_eval(cfg.get('experiment', 'numEpochs'))
-  minibatchSize = ast.literal_eval(cfg.get('experiment', 'minibatchSize'))
-  learningRate = ast.literal_eval(cfg.get('experiment', 'learningRate'))
-  rateDecay = ast.literal_eval(cfg.get('experiment', 'rateDecay'))
-  checkGradient = ast.literal_eval(cfg.get('experiment', 'checkGradient'))
-
-  # This option will continue the experiment for a certain number of epochs
-  # after we have gotten 0 training errors
-  numEpochsAfterOverfit = ast.literal_eval(cfg.get('experiment',
-    'numEpochsAfterOverfit'))
-  numEpochsRemaining = numEpochsAfterOverfit
-
-  # Set up the program options
-  debugMode = ast.literal_eval(cfg.get('program', 'debugMode'))
-  logToFile = ast.literal_eval(cfg.get('program', 'logToFile'))
-  logFileBaseName = cfg.get('program', 'logFileBaseName')
-  if logToFile:
-    dateStr = time.strftime('%Y-%m-%d_%H-%M')
-    # Add a UUID so we can track this experiment
-    uuidStr = uuid.uuid4()
-    logFile = logFileBaseName+"_"+dateStr+"_"+str(uuidStr)+".txt"
-    f = open(logFile, "w")
-    f.write('Num. Errors Train,Num. Errors Test,learningRate,momentum,elapsedTime\n')
-    # Also copy the params over for posterity
-    paramsCopyStr = logFileBaseName+"_params_"+str(uuidStr)+".ini"
-    shutil.copyfile(sys.argv[1], paramsCopyStr)
-
-  # Load the corresponding data
+  # Load MNIST data
+  digits = params.digits
+  mnistPath = params.datasetPath 
   X_tr, Y_tr, X_te, Y_te = bdp.loadMNISTnp(mnistPath, digits=digits,
       asBitVector=True)
 
-  print "Training for "+str(numEpochs)+" epochs:"
-  p = momentumInitial
-  for t in range(0, numEpochs):
-    if t == 2:
-      if checkGradient:
-        print "Checking gradient..."
-        mlp.check_gradient(X_tr[0:10, :], Y_tr[0:10, :], learningRate, 0)
-        print " Gradient checking complete."
-
-    startTime = time.time()
-
-    for i in range(0, X_tr.shape[0], minibatchSize):
-      mlp.train(X_tr[i:i+minibatchSize, :], Y_tr[i:i+minibatchSize],
-          learningRate, p)
+  for t in range(params.currentEpoch, params.totalEpochs):
+    print t
+    for i in range(0, X_tr.shape[0], params.minibatchSize):
+      model.train(X_tr[i:i+params.minibatchSize, :], Y_tr[i:i+params.minibatchSize])
 
 
-      if i%(1*minibatchSize)==0:
-        bdp.progBar(i, X_tr.shape[0])
-    bdp.progBar(X_tr.shape[0], X_tr.shape[0])
+  sys.exit(0)
+  #numEpochs = ast.literal_eval(cfg.get('experiment', 'numEpochs'))
+  #minibatchSize = ast.literal_eval(cfg.get('experiment', 'minibatchSize'))
+  #learningRate = ast.literal_eval(cfg.get('experiment', 'learningRate'))
+  #rateDecay = ast.literal_eval(cfg.get('experiment', 'rateDecay'))
+  #checkGradient = ast.literal_eval(cfg.get('experiment', 'checkGradient'))
 
-    elapsedTime = (time.time()-startTime)
-    print " Epoch {0}, learning rate: {1:.4f}, momentum: {2:.4f} elapsed time: {3:.2f}s".format(t, learningRate, p, elapsedTime)
 
-    # Decay the learning rate
-    learningRate = learningRate*rateDecay
+  # Load the parameters for this network from the initialization file
+  #cfg = ConfigParser.ConfigParser()
+  #cfg.read(sys.argv[1])
 
-    # Update the momentum
-    if t < momentumT:
-      p = (1.0-float(t)/momentumT)*momentumInitial + (float(t)/momentumT)*momentumFinal
-    else:
-      p = momentumFinal
+  #layerSizes = list(ast.literal_eval(cfg.get('net', 'layerSizes')))
+  #activations = cfg.get('net', 'activations').split(',')
+  #dropoutType = cfg.get('net', 'dropoutType')
+  #if dropoutType == 'nodropout':
+    #doDropout = False
+  #else:
+    #doDropout = True
+  #dropoutProb = ast.literal_eval(cfg.get('net', 'dropoutProb'))
+  #dropoutInputProb = ast.literal_eval(cfg.get('net', 'dropoutInputProb'))
+  #wLenLimit = ast.literal_eval(cfg.get('net', 'wLenLimit'))
+  #momentumInitial = ast.literal_eval(cfg.get('net', 'momentumInitial'))
+  #momentumFinal = ast.literal_eval(cfg.get('net', 'momentumFinal'))
+  #momentumT = ast.literal_eval(cfg.get('net', 'momentumT'))
 
-    # Calculate errors
-    YhatTrain = mlp.test(X_tr)
-    numErrsTrain = numErrs(Y_tr, YhatTrain)
-    YhatTest = mlp.test(X_te)
-    numErrsTest = numErrs(Y_te, YhatTest)
+  #mlp = MLP.MLP(layerSizes, 
+      #activations, 
+      #doDropout=doDropout, 
+      #dropoutType=dropoutType, 
+      #dropoutProb=dropoutProb, 
+      #dropoutInputProb=dropoutInputProb,
+      #wLenLimit=wLenLimit)
 
-    errsStr = "Train errors: {0}\n".format(numErrsTrain)
-    errsStr += "Test errors: {0}\n".format(numErrsTest)
-    print errsStr
 
-    logStr = "{0},{1},{2},{3},{4:.2f}\n".format(
-                numErrsTrain, numErrsTest, learningRate, p, elapsedTime)
-    if logToFile:
-      f.write(logStr)
-      f.flush()
+  # This option will continue the experiment for a certain number of epochs
+  # after we have gotten 0 training errors
+  #numEpochsAfterOverfit = ast.literal_eval(cfg.get('experiment',
+    #'numEpochsAfterOverfit'))
+  #numEpochsRemaining = numEpochsAfterOverfit
 
-    if numErrsTrain == 0 and numEpochsAfterOverfit > 0:
-      print "No training errors. Continuing for {0} more epochs.".format(numEpochsRemaining) 
-      numEpochsAfterOverfit -= 1
-    elif numErrsTrain == 0 and numEpochsRemaining == 0:
-      print "No training errors. Stopping."
-      break
-    elif numErrsTrain > 0:
-      numEpochsRemaining = numEpochsAfterOverfit
+  # Set up the program options
+  #debugMode = ast.literal_eval(cfg.get('program', 'debugMode'))
+  #logToFile = ast.literal_eval(cfg.get('program', 'logToFile'))
+  #logFileBaseName = cfg.get('program', 'logFileBaseName')
+  #if logToFile:
+    #dateStr = time.strftime('%Y-%m-%d_%H-%M')
+    ## Add a UUID so we can track this experiment
+    #uuidStr = uuid.uuid4()
+    #logFile = logFileBaseName+"_"+dateStr+"_"+str(uuidStr)+".txt"
+    #f = open(logFile, "w")
+    #f.write('Num. Errors Train,Num. Errors Test,learningRate,momentum,elapsedTime\n')
+    ## Also copy the params over for posterity
+    #paramsCopyStr = logFileBaseName+"_params_"+str(uuidStr)+".ini"
+    #shutil.copyfile(sys.argv[1], paramsCopyStr)
 
-  if logToFile:
-    f.close()
+
+  #print "Training for "+str(numEpochs)+" epochs:"
+  #p = momentumInitial
+  #for t in range(0, numEpochs):
+    #if t == 2:
+      #if checkGradient:
+        #print "Checking gradient..."
+        #mlp.check_gradient(X_tr[0:10, :], Y_tr[0:10, :], learningRate, 0)
+        #print " Gradient checking complete."
+
+    #startTime = time.time()
+
+    #for i in range(0, X_tr.shape[0], minibatchSize):
+      #mlp.train(X_tr[i:i+minibatchSize, :], Y_tr[i:i+minibatchSize],
+          #learningRate, p)
+
+
+      #if i%(1*minibatchSize)==0:
+        #bdp.progBar(i, X_tr.shape[0])
+    #bdp.progBar(X_tr.shape[0], X_tr.shape[0])
+
+    #elapsedTime = (time.time()-startTime)
+    #print " Epoch {0}, learning rate: {1:.4f}, momentum: {2:.4f} elapsed time: {3:.2f}s".format(t, learningRate, p, elapsedTime)
+
+    ## Decay the learning rate
+    #learningRate = learningRate*rateDecay
+
+    ## Update the momentum
+    #if t < momentumT:
+      #p = (1.0-float(t)/momentumT)*momentumInitial + (float(t)/momentumT)*momentumFinal
+    #else:
+      #p = momentumFinal
+
+    #MLP.save_model(mlp, None, "test.pkl")
+    ## Calculate errors
+    #YhatTrain = mlp.test(X_tr)
+    #numErrsTrain = numErrs(Y_tr, YhatTrain)
+    #YhatTest = mlp.test(X_te)
+    #numErrsTest = numErrs(Y_te, YhatTest)
+
+    #errsStr = "Train errors: {0}\n".format(numErrsTrain)
+    #errsStr += "Test errors: {0}\n".format(numErrsTest)
+    #print errsStr
+
+    #logStr = "{0},{1},{2},{3},{4:.2f}\n".format(
+                #numErrsTrain, numErrsTest, learningRate, p, elapsedTime)
+    #if logToFile:
+      #f.write(logStr)
+      #f.flush()
+
+    #if numErrsTrain == 0 and numEpochsAfterOverfit > 0:
+      #print "No training errors. Continuing for {0} more epochs.".format(numEpochsRemaining) 
+      #numEpochsAfterOverfit -= 1
+    #elif numErrsTrain == 0 and numEpochsRemaining == 0:
+      #print "No training errors. Stopping."
+      #break
+    #elif numErrsTrain > 0:
+      #numEpochsRemaining = numEpochsAfterOverfit
+
+  #if logToFile:
+    #f.close()
 
